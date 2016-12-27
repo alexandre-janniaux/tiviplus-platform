582142c9d6a00a6637a73c0fffd96a0836f61e1e
Alexandre JANNIAUX <alexandre.janniaux@telecom-paristech.org>
2016-12-27 13:30:39 +0100

    Add loans to admin.

--- tiviplus/planning/admin.py
+++ tiviplus/planning/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from planning.models import MaterialLoan
+from planning.models import MaterialLoan, Event
 
 class MaterialLoanAdmin(admin.ModelAdmin):
     pass
 
+class EventAdmin(admin.ModelAdmin):
+	pass
+
 admin.site.register(MaterialLoan)
+admin.site.register(Event)
 
 
